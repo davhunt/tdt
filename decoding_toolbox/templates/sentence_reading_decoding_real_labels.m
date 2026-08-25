a8bfde7294cf5165376e3ab1431673bca4d2c6f0
David Hunt <davhunt@iu.edu>
2026-08-25 16:58:35 -0400

    add sentence reading script

---
+++ decoding_toolbox/templates/sentence_reading_decoding_real_labels.m
@@ -0,0 +1,133 @@
+function [] = sentence_reading_decoding_real_labels(subject_num, input_dir, window_type, contrast, results_dir)
+
+% This function conducts a searchlight MVPA decoding analysis on the sentence reading task beta maps,
+% wherein a machine learning classifier is trained to distinguish, from a subject's BOLD activity patterns,
+% whether a given pattern corresponds to one trial type or another. The trial type contrast can be active vs. passive,
+% conjoined-active vs. object-relative, or grammatical vs. ungrammatical. The performance of the decoding classifier
+% for a given searchlight can then be used as a measure of how much information about the contrast is contained in
+% that searchlight's neural activity patterns across the experiment.
+
+% Four runs/blocks of data were collected for this experiment, with trial types balanced across all blocks, and so a
+% leave-one-run-out (LORO) cross-validation is performed where a classifier is trained on three blocks, and tested on
+% held out block.
+
+% subject_num: Subject number of subject, will be converted to string
+% input_dir: Directory under which we can find subject folders
+% window_type: Directory under subject folders, what type of window was used to calculate betas
+% contrast: The contrast to run MVPA decoding analysis on, can be "AP," "CAOR," or "GUG"
+% results_dir: Directory under window_type to save results in (e.g. "TDT_results")
+
+% Notes:
+% For contrasts "AP" or "CAOR" only use grammatical trials, while "GUG" use all trials
+
+tStart = tic;
+
+if exist('/N/slate/brainevo/Implicit_Learning') == 7
+    base_folder = ['/N/slate/brainevo/Implicit_Learning']; % If on slate
+elseif exist('/Volumes/data1') == 7
+    base_folder = ['/Volumes/data1']; % Or on habilis
+else
+    error('Can''t find base folder, are we on habilis or the HPC?');
+end
+
+if exist([base_folder '/tdt_3.999I/decoding_toolbox']) == 7 % slate
+    addpath(genpath([base_folder '/tdt_3.999I/decoding_toolbox']));
+else
+    addpath(genpath('/Users/lab/Downloads/tdt_3.999I/decoding_toolbox')) % habilis
+end
+if exist([base_folder '/spm12']) == 7
+    addpath(genpath([base_folder '/spm12']));
+else
+    addpath(genpath('/Users/lab/Downloads/spm12'))
+end
+
+cfg = decoding_defaults;
+
+spreadsheetFile = '/Volumes/data1/Implicit_Learning/sentence_reading_analysis/Sentence reading_newtrialproc_102319_edited by MY for SPM_Tom_edit_for_LSS.xlsx';
+sheetName = 'SPM_events';
+subjChar = num2str(subject_num, '%02d'); % later filepaths require two digit subject IDs
+T = readtable(spreadsheetFile, "Sheet", sheetName);
+T = T(T.subject == subject_num, :);
+blocks = unique(T.scanner_sequence_number, 'stable');
+if numel(blocks) ~= 4
+    error('Number of blocks should be 4, seeing %d.', numel(blocks));
+end
+
+
+%labelvalue1 = 1; % value for active, conjoined active, or grammatical
+%labelvalue2 = -1; % value for passive, object relative, or non-grammatical
+if contrast == "AP"
+    % keep grammatical, active/passive trials
+    T = T(matches(T.grammar_type, ["A","P"]),:);
+    T = T(T.grammaticality == "G", :);
+    labels = double(T.grammar_type == "A");
+    labels(labels == 0) = -1;
+elseif contrast == "CAOR"
+    % keep grammatical, CA/OR trials
+    T = T(matches(T.grammar_type, ["CA","OR"]),:);
+    T = T(T.grammaticality == "G", :);
+    labels = double(T.grammar_type == "CA");
+    labels(labels == 0) = -1;
+elseif contrast == "GUG"
+    % keep all trials
+    labels = double(T.grammaticality == "G");
+    labels(labels == 0) = -1;
+else
+    error('Contrast must be "AP", "CAOR", or "GUG".')
+end
+
+num_betas = size(T,1);
+names = cell(num_betas,1); chunks = zeros(num_betas,1);
+for i = 1:size(T,1)
+    sentence_id = char(T.sentence_ID(i));
+    names{i} = fullfile(input_dir, subjChar, window_type, ['beta_' sentence_id '.nii']);
+    chunks(i) = find(blocks == T.scanner_sequence_number(i));
+    if ~isfile(names{i})
+        error('Can''t find file %s.', names{i});
+    end
+end
+
+cfg.files.name = names;
+cfg.files.label = labels;
+cfg.files.chunk = chunks;
+cfg.files.mask = ['/Users/lab/Downloads/ants_registrations/sentence_reading/' subjChar '/mask_epi.nii.gz'];
+
+cfg.design = make_design_cv(cfg);
+
+cfg.plot_design = 0; % no plot in parfor
+cfg.verbose = 0;
+
+% Set the analysis that should be performed (default is 'searchlight')
+cfg.analysis = 'searchlight'; % standard alternatives: 'wholebrain', 'ROI' (pass ROIs in cfg.files.mask, see below)
+cfg.searchlight.radius = 3; % use searchlight of radius 3 (by default in voxels), see more details below
+cfg.searchlight.spherical = 1;
+cfg.scale.method = 'min0max1';
+cfg.scale.estimation = 'all'; % scaling across all data is equivalent to no scaling (i.e. will yield the same results), it only changes the data range which allows libsvm to compute faster
+cfg.results.output = {'accuracy_minus_chance','confusion_matrix'};
+% Set the output directory where data will be saved, e.g. 'c:\exp\results\buttonpress'
+cfg.results.dir = char(fullfile(input_dir,subjChar,window_type,results_dir));
+if ~isfolder(cfg.results.dir)
+    mkdir(cfg.results.dir)
+end
+
+results = decoding(cfg);
+
+%U = T(matches(T.grammar_type, ["A","P"]),:);
+
+
+%file_list = dir('/Volumes/data1/Implicit_Learning/sentence_reading_lss/01/windowType1/*beta*.nii');
+%file_list = dir(fullfile(input_dir, subjChar, window_type, '*beta*.nii'));
+%beta_files = {file_list.name}'; % cfg.files.name
+
+
+
+%[filepath,name,ext] = fileparts(file_list(1).name);
+%pattern = '^beta_([0-9]{2})([A-Z]+)$';
+%tokens = regexp(name, pattern, 'tokens')
+
+
+
+tEnd = toc(tStart);
+disp(['Running LORO CV on subject ' subjChar ' took ' num2str(tEnd, '%.2f') ' seconds. Done.'])
+
+end
